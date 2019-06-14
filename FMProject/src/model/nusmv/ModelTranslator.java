30b3cc005cffb8f0cc7f7b76abf06a7ffa6bd706
Francesco <ditommaso.1647819@studenti.uniroma1.it>
2019-06-14 19:07:58 +0200

    Modifiche
    1) Creazione metodi scrittura moduli

--- FMProject/src/model/nusmv/ModelTranslator.java
+++ FMProject/src/model/nusmv/ModelTranslator.java
@@ -7,6 +7,8 @@ public class ModelTranslator {
 
     private Session session;
 
+    private static String tabspace = "    ";
+
     /**
      * The class generate the NuSMV code from the Session object.
      * @return the NuSMV code as a string.
@@ -16,10 +18,51 @@ public class ModelTranslator {
 
         String nusmvCode = "";
 
-
+        String userModule = generateUserModule();
+        String roleModule = generateRoleModule();
+        String userRoleModule = generateUserRoleModule();
 
         return null;
     }
 
+    private static String generateModule(String name, String[] variables, String[] domains, String[] assignment){
+        String module = "MODULE " + name + "(";
+
+        for(int i = 0; i < variables.length; i++){
+            String var = variables[i];
+            module = module.concat("new" + var);
+            if(i < variables.length - 1){
+                module = module.concat(", ");
+            }
+        }
+
+        module = module.concat(")\n");
+        module = module.concat(tabspace + "VAR\n");
+        for(int i = 0; i < variables.length; i++){
+            module = module.concat(tabspace + tabspace + variables[i] + " : " + domains[i] + ";\n");
+        }
+
+        module = module.concat(tabspace + "ASSIGN\n");
+        for (int i = 0; i < variables.length; i++) {
+            String variable = variables[i];
+            module = module.concat(tabspace + tabspace + "init(" + variable + ") := new" + assignment[i] + ";\n");
+        }
+
+        return module;
+    }
+
+    private static String generateUserModule(){
+        return generateModule("user", new String[]{"id"}, new String[]{"0..100"}, new String[]{"id"});
+    }
+
+    private static String generateRoleModule(){
+        return generateModule("role", new String[]{"id"}, new String[]{"0..100"}, new String[]{"id"});
+    }
+
+    private static String generateUserRoleModule(){
+        return generateModule("userRole", new String[]{"user", "role", "state", "active"},
+                new String[]{"0..100", "0..100", "{notAut, Aut}", "boolean"},
+                new String[]{"user", "role", "state", "active"});
+    }
 
 }
