effcbf4dba5a2040205a72838d922dfddbabb4c5
Francesco <ditommaso.1647819@studenti.uniroma1.it>
2019-06-18 18:06:38 +0200

    Modifiche
    1) Piccole modifiche

--- FMProject/src/model/nusmv/FileWriter.java
+++ FMProject/src/model/nusmv/FileWriter.java
@@ -6,7 +6,12 @@ public class FileWriter {
 
     public static void main(String[] args){
         String code = ModelTranslator.fromModelToNuSMV("C:\\Users\\Francesco\\IdeaProjects\\ProjectFM\\FMProject\\hospital.xml");
-        System.out.println(code);
+        if(code != null){
+            System.out.println("No file");
+        } else {
+            System.out.println(code);
+        }
+
     }
 
 }
